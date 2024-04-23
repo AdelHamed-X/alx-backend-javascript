ff0243d4db3ea7edf5676c9499fab1f9c5c6ac36
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-23 12:56:08 +0200

    Adding Task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    'income': income, // eslint-disable-line
-    'gdp': gdp, // eslint-disable-line
-    'capita': capita, // eslint-disable-line
+    income,
+    gdp,
+    capita,
   };
 
   return budget;
