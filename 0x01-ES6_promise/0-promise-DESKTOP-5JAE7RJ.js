c7fc9dcf6948721ac795cd92fdda7e8b87f4df51
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-25 04:48:18 +0200

    Adding Task 0

---
+++ 0x01-ES6_promise/0-promise-DESKTOP-5JAE7RJ.js
@@ -0,0 +1,5 @@
+function getResponseFromAPI () {
+  let promise = new Promise() {
+    
+  }
+}
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI () {
+  let promise = new Promise((resolveFunc, rejectFunc) => {
+    
+  }
+
+  );
+}
