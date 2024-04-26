9ab0085fad6300a9fb8c17a87f508056a0c184b4
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-26 23:08:24 +0300

    Adding task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof val === "number") {
+      this._amount = val;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    if (val instanceof Currency) {
+      this._currency = val;
+    }
+  }
+
+  displayfullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency}`;
+  }
+
+  converPrice(amount, converionRate) {
+    return amount * converionRate;
+  }
+}
