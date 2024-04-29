9a64b7d02c863913e1426ef49ed3e5bc7ebe62e4
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-04-29 19:20:52 +0300

    Adding Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,17 @@
-export default function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstname: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstname: "James",
-      location: "Columbia",
-    },
-    {
-      id: 5,
-      firstname: "Serena",
-      location: "San Francisco",
-    },
-  ];
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstname: "Guillaume",
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    firstname: "James",
+    location: "Columbia",
+  },
+  {
+    id: 5,
+    firstname: "Serena",
+    location: "San Francisco",
+  },
+];
